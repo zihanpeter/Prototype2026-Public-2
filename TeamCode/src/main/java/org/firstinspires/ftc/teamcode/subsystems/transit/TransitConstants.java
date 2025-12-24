4b9af94df8cdf7d9dff22f0c7aeed3ee5c338142
YCY5449 <469779590@qq.com>
2025-12-25 02:29:09 +0800

    new TransitServo before SH1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/transit/TransitConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/transit/TransitConstants.java
@@ -2,19 +2,10 @@ package org.firstinspires.ftc.teamcode.subsystems.transit;
 
 import com.acmerobotics.dashboard.config.Config;
 
-/**
- * Constants for the Transit subsystem.
- */
 @Config
 public class TransitConstants {
-    // Hardware map name for the transit servo
     public static String transitServoName = "transitServo";
 
-    // Servo positions
-    public static double transitUpPos = 0.94;   // Position to push element into shooter
-    public static double transitDownPos = 0.74; // Retracted position
-
-    // Duration (in seconds) for the pulse logic (used in FAST mode)
-    // Servo lifts for this duration then retracts
-    public static double pulseDuration = 0.1;
+    public static double transitUpPos = 0.87;
+    public static double transitDownPos = 0.67;
 }
